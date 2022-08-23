81fb67f6956ad35523c0a2f42de39233d8d55c4f
ghufran919 <ahmadghufranakbar@gmail.com>
2022-08-24 06:15:25 +0800

    server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+//TMDB 
+
+const API_KEY = 'api_key=d28fb04dfed044d5c72f19bfff4ed459';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
